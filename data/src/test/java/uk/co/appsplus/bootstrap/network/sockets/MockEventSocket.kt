b6ebe7f5faab15a8d9cb595bcbe664d3d28b9d72
Rich <richard.clements@image-plus.co.uk>
2022-05-26 18:51:31 +0100

    Added MockEventSocket

---
+++ data/src/test/java/uk/co/appsplus/bootstrap/network/sockets/MockEventSocket.kt
@@ -0,0 +1,19 @@
+package uk.co.appsplus.bootstrap.network.sockets
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import uk.co.appsplus.bootstrap.network.models.sockets.SocketChannel
+import uk.co.appsplus.bootstrap.network.models.sockets.SocketEvent
+import uk.co.appsplus.bootstrap.network.models.sockets.SocketMessage
+
+class MockEventSocket : EventSocket {
+
+    var subscribedChannel: SocketChannel? = null
+    var subscribedEvents: List<SocketEvent>? = null
+    var subscriptionFlow: Flow<SocketMessage> = flow {}
+    override fun subscribe(channel: SocketChannel, events: List<SocketEvent>): Flow<SocketMessage> {
+        subscribedChannel = channel
+        subscribedEvents = events
+        return subscriptionFlow
+    }
+}
