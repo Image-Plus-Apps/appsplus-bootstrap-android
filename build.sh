3dd5cf3a6e7e61c2dd3732bb2709ebff5bf926c4
Rich <richard.clements@image-plus.co.uk>
2022-08-11 14:49:26 +0100

    Added buildAll task to compile locally

---
+++ build.sh
@@ -0,0 +1,6 @@
+./gradlew data:assembleRelease -Pversion="1"
+./gradlew data:publishToMavenLocal -Pversion="1"
+./gradlew ui:assembleRelease -Pversion="1"
+./gradlew ui:publishToMavenLocal -Pversion="1"
+./gradlew testing:assembleRelease -Pversion="1"
+./gradlew testing:publishToMavenLocal -Pversion="1"
